a542d90510756ff266a11269cfd60f85a27e0264
Maxim Brett <brett.maxim@mail.ru>
2026-02-08 23:01:33 +0400

    test errors

--- lab1_intro/main.cpp
+++ lab1_intro/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
+#include <vector>
 
-int             main()         {
-    // 1. Ошибка стиля: Google Style требует 2 пробела, а тут хаос.
-    int*       leak = new int[10]; // 2. Утечка памяти: выделили через new, но не удалили.
 
-    for(int i=0; i<10; i++){
-        leak[i] = i * 2;
-        std::cout << "Value: " << leak[i] << std::endl;
+int main() {
+    std::vector<int> data(10);
+
+    for (int i = 0; i < 10; i++) {
+        data[i] = i * 2;
+        std::cout << "Value: " << data[i] << std::endl;
     }
 
-    // 3. Ошибка стиля: отсутствие пустой строки в конце файла и лишние пробелы.
     return 0;
 }
