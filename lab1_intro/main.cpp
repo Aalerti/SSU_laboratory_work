6f1b90a1ab56d5adc6489180e640489c883e003d
Maxim Brett <brett.maxim@mail.ru>
2026-02-08 22:32:03 +0400

    test CI

--- lab1_intro/main.cpp
+++ lab1_intro/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-int main() {
-    std::cout << "Hello, Big Tech!" << std::endl;
+
+int             main()         {
+    // 1. Ошибка стиля: Google Style требует 2 пробела, а тут хаос.
+    int*       leak = new int[10]; // 2. Утечка памяти: выделили через new, но не удалили.
+
+    for(int i=0; i<10; i++){
+        leak[i] = i * 2;
+        std::cout << "Value: " << leak[i] << std::endl;
+    }
+
+    // 3. Ошибка стиля: отсутствие пустой строки в конце файла и лишние пробелы.
     return 0;
 }
